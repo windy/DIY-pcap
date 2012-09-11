2de7c6c23a7dd16703e560c66f7318951cf14735
yafei Lee <lyfi2003@gmail.com>
2012-09-11 15:58:15 +0800

    simple example fix

--- simple/diy-strategy-pcap.rb
+++ simple/diy-strategy-pcap.rb
@@ -1,6 +1,6 @@
 class NoMacEqualStrategy < DIY::BasicStrategy
   def call(hope_pkt, recv_pkt, queue)
-    return OK if hope_pkt[11..-1] == recv_pkt[11..-1]
+    return OK if hope_pkt[12..-1] == recv_pkt[12..-1]
     return NONE
   end
 end
@@ -14,7 +14,7 @@ end
 
 builder = DIY::Builder.new do
   use nomac
-  before_send change_mac
+  before_send &change_mac
   pcapfile "pcaps/gre.pcap"
 end
 
